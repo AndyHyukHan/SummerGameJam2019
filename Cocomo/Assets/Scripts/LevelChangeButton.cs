bc0df81817c5a74cd5e20e54e95bdc82a0ad97c9
ANDY HAN <ANDY.HAN.DP@tumwater.k12.wa.us>
2019-06-05 13:33:59 -0700

    Added UI buttons

---
+++ Cocomo/Assets/Scripts/LevelChangeButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChangeButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate is called once per frame for physics effects
+    void FixedUpdate()
+    {
+
+    }
+}
